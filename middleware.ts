b5e0b4e38658979f6355c3bb0a985fe54fde2eaf
Oliver Beeny <oliverbeeny@Olivers-iMac.lan>
2025-11-12 23:44:41 +0000

    Remove middleware file completely

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export async function middleware(request: NextRequest) {
-  // Middleware is completely disabled - just pass through all requests
-  return NextResponse.next();
-}
-
-// Don't export config to let Next.js handle routing normally without middleware interference
